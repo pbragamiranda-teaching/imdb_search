a111468bf17672e2cb9c42204627cf3088238e29
Pedro Miranda <pbragamiranda@gmail.com>
2020-12-07 09:13:57 -0300

    models + seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "yaml"
+
+file = "https://gist.githubusercontent.com/pbragamiranda/2e7b4954503c088f474ab4d16d8389dd/raw/5c46f34096dcb01fc15b0abb893a6371616b6340/imdb.yml"
+sample = YAML.load(open(file).read)
+
+puts 'Creating directors...'
+directors = {}  # slug => Director
+sample["directors"].each do |director|
+  directors[director["slug"]] = Director.create! director.slice("first_name", "last_name")
+end
+
+puts 'Creating movies...'
+sample["movies"].each do |movie|
+  Movie.create! movie.slice("title", "year", "synopsis").merge(director: directors[movie["director_slug"]])
+end
+
+puts 'Creating tv shows...'
+sample["series"].each do |tv_show|
+  TvShow.create! tv_show
+end
+puts 'Finished!'
